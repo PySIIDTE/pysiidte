c9c2b9bb0e0f7a37b4f497b946369dc4a8514766
Daniel Blanco <daniel@blancomartin.cl>
2017-05-06 21:45:56 -0300

    change .py extension

---
+++ __init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"""Módulo auxiliares y para realizar conexión, firma y servicios
+   relacionados don Documentos Tributarios Electrónicos del SII
+   (Servicio de Impuestos Internos) de Chile
+"""
+__author__ = "Daniel Blanco Martín (daniel@blancomartin.cl)"
+__copyright__ = "Copyright (C) 2015-2017 Blanco Martín y Asoc. EIRL - BMyA S.A."
+__license__ = "AGPL 3.0"
--- setup.py
+++ setup.py
@@ -1,8 +1,67 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"Utilidad para instalación de paquete de pysiidte"
+
+__author__ = "Daniel Blanco Martín (daniel@blancomartin.cl)"
+__copyright__ = "Copyright (C) 2015-2017 Blanco Martín y Asoc. EIRL - BMyA S.A."
+__license__ = "AGPL 3.0"
+
+from setuptools import setup, find_packages
+import glob
+import os
+import subprocess
+import warnings
+import sys
+
+
 
 setup(
     name='pysiidte',
     version='0.1',
-    scripts=['pysiidte']
+    url='https://bitbucket.org/hdblanco/pysiidte',
+    license='AGPL-3',
+    author='Daniel Blanco Martín',
+    author_email='daniel@blancomartin.cl',
+    description='Utilidades para firma y conexión SII Chile',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        # 'lxml >= 3.6.0, < 3.7',
+        # 'defusedxml >= 0.4.1, < 0.5',
+        # 'eight >= 0.3.0, < 0.4',
+        # 'cryptography >= 1.4, < 1.5',
+        # 'pyOpenSSL >= 16.0.0, < 17',
+        # 'certifi >= 2015.11.20.1',
+        'signxml',
+        'logging',
+    ],
+    extras_require={
+        ':python_version == "2.7"': ['enum34 >= 1.0.4'],
+        ':python_version == "3.3"': ['enum34 >= 1.0.4']
+    },
+    packages=find_packages(exclude=['test']),
+    platforms=['MacOS X', 'Posix'],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGPL-3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
