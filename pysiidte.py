12b4aeea142c053cb94cda69024bbc1ff033b115
Daniel Blanco <daniel@blancomartin.cl>
2017-05-06 21:46:59 -0300

    change .py extension

--- pysiidte.py
+++ pysiidte.py
@@ -1,9 +1,23 @@
-#!/home/daniel/odoo-ve/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-##############################################################################
-# Nuevo pysiidte probando a llevarme todas las funciones esenciales          #
-# 2017-05-06                                                                 #
-##############################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTIBILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+
+"""Módulo auxiliares y para realizar conexión, firma y servicios
+   relacionados don Documentos Tributarios Electrónicos del SII
+   (Servicio de Impuestos Internos) de Chile
+"""
+__author__ = "Daniel Blanco Martín (daniel@blancomartin.cl)"
+__copyright__ = "Copyright (C) 2015-2017 Blanco Martín y Asoc. EIRL - BMyA S.A."
+__license__ = "AGPL 3.0"
+
 import ssl
 from lxml import etree
 from signxml import XMLSigner, XMLVerifier, methods
