9cbcddffb1935ea0a14ae7a6ae52233fc027ec44
Daniel Blanco <daniel@blancomartin.cl>
2017-08-13 20:36:39 -0300

    [FIX] Added Base64

--- pysiidte.py
+++ pysiidte.py
@@ -10,6 +10,7 @@
 # or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
 from __future__ import print_function
+import base64
 import collections
 import hashlib
 import logging
